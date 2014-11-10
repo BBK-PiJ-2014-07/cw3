aaee7db23997b83f927afecc54040db88274c1e4
skooni01 <skooni01@mail.bbk.ac.uk>
2014-11-10 09:26:10 +0000

    FunctionalArrayList updated

--- FunctionalArrayList.java
+++ FunctionalArrayList.java
@@ -1,26 +1,29 @@
 public class FunctionalArrayList extends ArrayList implements FunctionalList {
+	private static int DEFAULT_ARRAY_SIZE = 10;
+	private int arraySize;
+	private Object[] array;
 
 	public FunctionalArrayList(){
 		super();
 	}
 
 	public ReturnObject head() {
-		if(getArray()[0] == null){
+		if(array[0] == null){
 			return new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
 		} else {
-			return new ReturnObjectImpl(getArray()[0]);
+			return new ReturnObjectImpl(array[0]);
 		}
 	}
 
 	public FunctionalList rest() {
 		FunctionalArrayList newArray = new FunctionalArrayList();
-		for (int i=1; i<getArray().length; i++) {
-			if(getArray()[i] == null){
-			} else {	
-				newArray.add(getArray()[i]);
+		for (int i=1; i<array.length; i++) {
+			if(array[i] != null){
+				newArray.add(array[i]);
 			} 
 		}
 			return newArray;
 
 	}
+
 }
