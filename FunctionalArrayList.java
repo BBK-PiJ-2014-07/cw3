3f4b236369d33b405e4e6fc364443f73b15290b0
skooni01 <skooni01@mail.bbk.ac.uk>
2014-11-10 09:38:31 +0000

    Broke FunctionalArrayList, then un-broke it again.

--- FunctionalArrayList.java
+++ FunctionalArrayList.java
@@ -1,29 +1,26 @@
 public class FunctionalArrayList extends ArrayList implements FunctionalList {
-	private static int DEFAULT_ARRAY_SIZE = 10;
-	private int arraySize;
-	private Object[] array;
 
 	public FunctionalArrayList(){
 		super();
 	}
 
 	public ReturnObject head() {
-		if(array[0] == null){
+		if(getArray()[0] == null){
 			return new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
 		} else {
-			return new ReturnObjectImpl(array[0]);
+			return new ReturnObjectImpl(getArray()[0]);
 		}
 	}
 
 	public FunctionalList rest() {
 		FunctionalArrayList newArray = new FunctionalArrayList();
-		for (int i=1; i<array.length; i++) {
-			if(array[i] != null){
-				newArray.add(array[i]);
+		for (int i=1; i<getArray().length; i++) {
+			if(getArray()[i] == null){
+			} else {	
+				newArray.add(getArray()[i]);
 			} 
 		}
 			return newArray;
 
 	}
-
 }
