88c3a09d37b53c8092df9538c2f48eb0fb54a9cf
BBK-PIJ-2014-07 <skooni01@mail.bbk.ac.uk>
2014-11-11 14:48:11 +0000

    Completed StackImpl

---
+++ StackImpl.java
@@ -0,0 +1,40 @@
+/** 
+* Implementation of AbstractStack
+*/
+
+public class StackImpl extends AbstractStack {
+
+	public StackImpl(List list){
+		super(list);
+	}
+
+	public boolean isEmpty(){
+		return (internalList.get(0) == null) ?  true : false;
+	}
+
+	public int size(){
+		return internalList.size();
+	}
+
+	public void push(Object item){
+		internalList.add(item);	
+	}
+
+	public ReturnObject top(){
+		if (!isEmpty()){
+			return internalList.get(size()-1);
+		} else {
+			return new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
+		}
+	}
+
+	public ReturnObject pop(){
+		if (!isEmpty()) {		
+			return internalList.remove(size()-1);
+		} else {
+			return new ReturnObjectImpl(ErrorMessage.EMPTY_STRUCTURE);
+		}	
+	}
+
+
+}
